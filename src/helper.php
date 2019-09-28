17669734aa71b2e02dc9cee2f5724a98a248ff18
wuyanwen <wuyanwen@baijiayun.com>
2019-09-29 07:33:56 +0800

    修改助手函数

--- src/helper.php
+++ src/helper.php
@@ -1,12 +1,24 @@
 <?php
 if (!function_exists('createTableTimestamps')) {
-    function createTableTimestamps(\Illuminate\Database\Schema\Blueprint $table)
+    /**
+     * 创建
+     *
+     * @time 2019年09月29日
+     * @param \Illuminate\Database\Schema\Blueprint $table
+     * @param bool $softDelete
+     * @return void
+     */
+    function createTableTimestamps(\Illuminate\Database\Schema\Blueprint $table, $softDelete = false)
     {
         if (config('catchAdmin.model.unix_timestamp')) {
             $table->unsignedInteger('created_at')->default(0)->comment('创建时间');
             $table->unsignedInteger('updated_at')->default(0)->comment('更新时间');
         } else {
             $table->timestamps();
+        }
+
+        if ($softDelete) {
+            $table->softDeletes();
         }
     }
 }
